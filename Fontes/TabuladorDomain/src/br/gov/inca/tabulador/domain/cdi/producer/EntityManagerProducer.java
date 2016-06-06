ac7a51e99a26cb0436037443319361e7baea183d
rmanoel <rmanoel@IT-DES14.inca.local>
2016-06-06 14:41:33 -0300

    Criação das classes para CDI

---
+++ Fontes/TabuladorDomain/src/br/gov/inca/tabulador/domain/cdi/producer/EntityManagerProducer.java
@@ -0,0 +1,29 @@
+package br.gov.inca.tabulador.domain.cdi.producer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	private EntityManagerFactory factory;
+	
+	public EntityManagerProducer() {
+		this.factory = Persistence.createEntityManagerFactory("TabuladorWeb");
+	}
+	
+	@Produces
+	@RequestScoped
+	public EntityManager createEntityManager() {
+		return factory.createEntityManager();
+	}
+	
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		manager.close();
+	}
+}
